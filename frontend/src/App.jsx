dec324e82191ffbc1b4d601e81b51700cba8f5b6
Ian-Warugongo <ian.k.warugongo@gmail.com>
2025-08-26 14:15:40 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
