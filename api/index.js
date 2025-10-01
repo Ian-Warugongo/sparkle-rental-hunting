021130509997e504d042fb42d9516954098d3011
Ian-Warugongo <ian.k.warugongo@gmail.com>
2025-10-01 14:45:59 +0300

    created listing api route

--- api/index.js
+++ api/index.js
@@ -3,17 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
-
-mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected succesfully");        
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 
 const app = express();
 
@@ -28,6 +20,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
@@ -38,3 +31,12 @@ app.use((err, req, res, next) => {
         message,
     });
 });
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected successfully");        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
