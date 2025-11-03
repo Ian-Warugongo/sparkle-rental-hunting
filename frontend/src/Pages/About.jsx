34777437bd37ccf528bfbf104a69aa08d801d22f
Ian-Warugongo <ian.k.warugongo@gmail.com>
2025-11-03 19:50:43 +0300

    completed about page

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-blue-800'>About Sparkle Rentals</h1>
+      <p className='mb-4'>Welcome to Sparkle Rentals, your trusted destination for finding, listing, and managing rental properties with ease.</p>
+      <p className='mb-4'>At Sparkle Rentals, we're redefining the real estate experience through technology — offering a simple, secure, and user-friendly platform that connects renters and property owners seamlessly. Whether you're looking for your next home or listing a property, Sparkle Rentals makes the entire process efficient and transparent.</p>
+      <p className='mb-4'>Built with modern web technologies, our platform ensures fast performance, responsive design, and reliable data protection. With smart features like secure user authentication and real-time property updates, Sparkle Rentals brings convenience and confidence to every interaction.</p>
+      <p className='mb-4'>Our mission is to make renting smarter, faster, and more accessible — helping you find your perfect space with just a few clicks.</p>
+    </div>
   )
 }
 
