df65aeecfec3390f19e436a4cf11beb21050f0ba
Ian-Warugongo <ian.k.warugongo@gmail.com>
2025-11-06 17:34:32 +0300

    initial commit

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use('/', express.static(path.join(__dirname, '/frontend/dist')));
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
